c1396ae418a1dbbf017d071a0446c0bcfdaa237b
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2019-09-30 16:08:57 +0200

    ouput DN config

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -9,12 +9,16 @@ openssl genrsa -out admin-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in admin-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out admin-key.pem
 openssl req -new -key admin-key.pem -out admin.csr
 openssl x509 -req -in admin.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out admin.pem
+echo "Remember to add this to opendistro_security.authcz.admin_dn:"
+openssl x509 -subject -nameopt RFC2253 -noout -in admin.pem
 # Node cert
 echo "Generating node cert"
 openssl genrsa -out node-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in node-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out node-key.pem
 openssl req -new -key node-key.pem -out node.csr
 openssl x509 -req -in node.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out node.pem
+echo "Remember to add this to opendistro_security.nodes_dn:"
+openssl x509 -subject -nameopt RFC2253 -noout -in node.pem
 # Cleanup
 rm admin-key-temp.pem
 rm admin.csr
