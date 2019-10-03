ecdfc62770c51fdde6120e0d6c04a7616f546631
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2019-10-03 09:22:56 +0200

    improvements with security certs generation

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env bash
 # Root CA
+SCRIPTPATH=$(dirname $(realpath -s $0)) 
+echo $SCRIPTPATH
+cd $SCRIPTPATH/..
+mkdir -p .certs
+cd .certs
+
 echo "Generating root CA"
 openssl genrsa -out root-ca-key.pem 2048
 openssl req -new -x509 -sha256 -key root-ca-key.pem -out root-ca.pem
-# Admin cert
+
 echo "Generating admin cert"
 openssl genrsa -out admin-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in admin-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out admin-key.pem
 openssl req -new -key admin-key.pem -out admin.csr
 openssl x509 -req -in admin.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out admin.pem
+
+echo "--------------------------------------------------------------"
 echo "Remember to add this to opendistro_security.authcz.admin_dn:"
 openssl x509 -subject -nameopt RFC2253 -noout -in admin.pem
-# Node cert
+echo "--------------------------------------------------------------"
+
 echo "Generating node cert"
 openssl genrsa -out node-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in node-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out node-key.pem
 openssl req -new -key node-key.pem -out node.csr
 openssl x509 -req -in node.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out node.pem
+
+echo "--------------------------------------------------------------"
 echo "Remember to add this to opendistro_security.nodes_dn:"
 openssl x509 -subject -nameopt RFC2253 -noout -in node.pem
+echo "--------------------------------------------------------------"
+
 # Cleanup
 rm admin-key-temp.pem
 rm admin.csr
