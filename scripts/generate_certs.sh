123453b7c242031f75f268bee8079dc155481893
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2019-10-15 14:18:29 +0200

    secrets and helpers

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -6,14 +6,14 @@ cd $SCRIPTPATH/..
 mkdir -p .secrets
 cd .secrets
 
-echo "Generating root CA"
-openssl genrsa -out root-ca-key.pem 2048
+echo "Generating root CA, keep this key private and safe"
+openssl genrsa -out root-ca-key.pem 2048 -subj "/C=NO/ST=OSLO/L=OSLO/O=NAV/CN=ADMIN"
 openssl req -new -x509 -sha256 -key root-ca-key.pem -out root-ca.pem
 
 echo "Generating admin cert"
 openssl genrsa -out admin-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in admin-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out admin-key.pem
-openssl req -new -key admin-key.pem -out admin.csr
+openssl req -new -key admin-key.pem -out admin.csr -subj "/C=NO/ST=OSLO/L=OSLO/O=NAV/CN=ADMIN"
 openssl x509 -req -in admin.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out admin.pem
 
 echo "--------------------------------------------------------------"
@@ -24,7 +24,7 @@ echo "--------------------------------------------------------------"
 echo "Generating node cert"
 openssl genrsa -out node-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in node-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out node-key.pem
-openssl req -new -key node-key.pem -out node.csr
+openssl req -new -key node-key.pem -out node.csr -subj "/C=NO/ST=OSLO/L=OSLO/O=NAV/CN=NODE"
 openssl x509 -req -in node.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out node.pem
 
 echo "--------------------------------------------------------------"
@@ -32,8 +32,16 @@ echo "Remember to add this to opendistro_security.nodes_dn:"
 openssl x509 -subject -nameopt RFC2253 -noout -in node.pem
 echo "--------------------------------------------------------------"
 
+echo "Generating client cert"
+openssl genrsa -out client-key-temp.pem 2048
+openssl pkcs8 -inform PEM -outform PEM -in client-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out client-key.pem
+openssl req -new -key client-key.pem -out client.csr -subj "/C=NO/ST=OSLO/L=OSLO/O=NAV/CN=readall"
+openssl x509 -req -in client.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out client.pem
+
 # Cleanup
 rm admin-key-temp.pem
 rm admin.csr
 rm node-key-temp.pem
 rm node.csr
+rm client-key-temp.pem
+rm client.csr
---
+++ scripts/generate_kubernetes_secrets.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+SCRIPTPATH=$(dirname $(realpath -s $0))
+cd $SCRIPTPATH/..
+RELEASE_NAME=$1
+PASSWORD=$2
+echo "generate kubernetes secret for $RELEASE_NAME certificates" 
+helm template  -n $RELEASE_NAME  --set odfe.generate_secrets=true -x templates/odfe-cert-secrets.yaml . | kubectl apply -f -
+echo "encrypting password"
+HASH=$(docker run amazon/opendistro-for-elasticsearch sh /usr/share/elasticsearch/plugins/opendistro_security/tools/hash.sh -p $PASSWORD)
+echo "generate kubernetes secret for certificates $HASH"
+helm template -n stilling --set odfe.generate_secrets=true --set odfe.security.password.hash="$HASH" -x templates/odfe-config-secrets.yaml . | kubectl apply -f -
+echo "generating secret for kibana"
+helm template -n stilling --set odfe.generate_secrets=true --set kibana.password="$PASSWORD" -x templates/odfe-02-kibana-secrets.yaml . | kubectl apply -f -
