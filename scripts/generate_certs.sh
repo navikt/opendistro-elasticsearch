483c5f488250d4c7626e9bd9105e10a9200c3fdf
tuan <tdatuan1975@gmail.com>
2019-11-04 09:30:23 +0100

    extend certs expiration

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Root CA
+set -e
+
 SCRIPTPATH=$(dirname $(realpath -s $0)) 
 echo $SCRIPTPATH
 cd $SCRIPTPATH/..
@@ -8,13 +10,13 @@ cd .secrets
 
 echo "Generating root CA, keep this key private and safe"
 openssl genrsa -out root-ca-key.pem 2048 
-openssl req -new -x509 -sha256 -key root-ca-key.pem -out root-ca.pem -subj "/C=NO/ST=OSLO/L=OSLO/O=NAV/CN=ROOT"
+openssl req -new -x509 -sha256 -key root-ca-key.pem -out root-ca.pem -subj "/C=NO/ST=OSLO/L=OSLO/O=NAV/CN=ROOT" -days 3650
 
 echo "Generating admin cert"
 openssl genrsa -out admin-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in admin-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out admin-key.pem
 openssl req -new -key admin-key.pem -out admin.csr -subj "/C=NO/ST=OSLO/L=OSLO/O=NAV/CN=ADMIN"
-openssl x509 -req -in admin.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out admin.pem
+openssl x509 -req -in admin.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out admin.pem -days 3650
 
 echo "--------------------------------------------------------------"
 echo "Remember to add this to opendistro_security.authcz.admin_dn:"
@@ -25,7 +27,7 @@ echo "Generating node cert"
 openssl genrsa -out node-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in node-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out node-key.pem
 openssl req -new -key node-key.pem -out node.csr -subj "/C=NO/ST=OSLO/L=OSLO/O=NAV/CN=NODE"
-openssl x509 -req -in node.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out node.pem
+openssl x509 -req -in node.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out node.pem -days 3650
 
 echo "--------------------------------------------------------------"
 echo "Remember to add this to opendistro_security.nodes_dn:"
@@ -36,7 +38,7 @@ echo "Generating monitor cert"
 openssl genrsa -out monitor-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in monitor-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out monitor-key.pem
 openssl req -new -key monitor-key.pem -out monitor.csr -subj "/C=NO/ST=OSLO/L=OSLO/O=NAV/CN=monitor"
-openssl x509 -req -in monitor.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out monitor.pem
+openssl x509 -req -in monitor.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out monitor.pem -days 3650
 
 # Cleanup
 rm admin-key-temp.pem
